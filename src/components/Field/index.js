2ed527b28791c075fa164b103b454744e48704b1
Hugo Fabricio <me@hugofabricio.com>
2019-10-27 13:32:20 -0300

    Update input component

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,6 +1,3 @@
-import Group from "./Group";
-import Feedback from "./Feedback";
-import Label from "./Label";
 import Input from "./Input";
 
-export { Group, Label, Input, Feedback };
+export default Input;
